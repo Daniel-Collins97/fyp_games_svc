2629328385f50c28bcf01e32fed285c081b0be6f
Daniel-Collins97 <daniel.collins@routematch.com>
2019-08-08 16:56:19 +0100

    Updated to use different port

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const db = require('./queries.js');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgress API' })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+app.get('/games', db.getGames);
+app.get('/games/:game_id', db.getGamesById);
+
+app.post('/games', db.createGame);
+
+app.put('/games/:game_id', db.updateGame);
+
+app.delete('/games/:game_id', db.deleteGame);
---
+++ queries.js
@@ -0,0 +1,70 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: 'daniel',
+  host: 'localhost',
+  database: 'finalyearprojectdb',
+  password: 'Daniel_joseph1',
+  port: 5432,
+})
+
+const getGames = (request, response) => {
+  pool.query('SELECT * FROM "Games"', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  })
+}
+
+const getGamesById = (request, response) => {
+  const game_id = parseInt(request.params.game_id);
+
+  pool.query('SELECT * FROM "Games" WHERE "game_id" = $1', [game_id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createGame = (request, response) => {
+  const { game_id, date, location, opposition, conditions } = request.body;
+
+  pool.query('INSERT INTO "Games" ("game_id", "date", "location", "opposition", "conditions") VALUES ($1, $2, $3, $4, $5)', [game_id, date, location, opposition, conditions], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).send(`Game Added with ID: ${game_id}`)
+  })
+}
+
+const updateGame = (request, response) => {
+  const game_id = parseInt(request.params.game_id);
+  const { location } = request.body;
+
+  pool.query('UPDATE "Games" SET "location" = $1 WHERE "game_id" = $2', [location, game_id], (error) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`Game modified with game_id: ${game_id}`);
+  })
+}
+
+const deleteGame = (request, response) => {
+  const game_id = parseInt(request.params.game_id);
+
+  pool.query('DELETE FROM "Games" WHERE "game_id" = $1', [game_id], (error) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`Game deleted with game_id: ${game_id}`)
+  })
+}
+
+module.exports = {
+  getGames,
+  getGamesById,
+  createGame,
+  updateGame,
+  deleteGame,
+}
